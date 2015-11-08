2c64e2affb6cb04f3e278b385c34ee604d134ff6
Amy Bruderer <amy.bruderer@gmail.com>
2015-11-07 20:53:33 -0500

    Implemented load.

--- pset5/dictionary.c
+++ pset5/dictionary.c
@@ -7,9 +7,23 @@
  * Implements a dictionary's functionality.
  ***************************************************************************/
 
+#include <ctype.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "dictionary.h"
+#include "node.h"
+
+// children per node (one for every lowercase character and apostrophe)
+#define CHILDREN 27
+
+// prototypes
+void add_word(const char* word);
+int get_node_index(char c);
+
+// in memory dictionary
+node tree;
 
 /**
  * Returns true if word is in dictionary else false.
@@ -25,8 +39,27 @@ bool check(const char* word)
  */
 bool load(const char* dictionary)
 {
-    // TODO
-    return false;
+    // open dictionary file
+    FILE* file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        printf("Could not open %s.\n", dictionary);
+        return false;
+    }
+
+    // initialize dictionary
+    init_node(&tree, CHILDREN);
+
+    char word[LENGTH+1];
+
+    // load each word from dictionary file
+    while (EOF != fscanf(file, "%s", word))
+    {
+        // add word to dictionary
+        add_word(word);
+    }
+
+    return true;
 }
 
 /**
@@ -45,4 +78,63 @@ bool unload(void)
 {
     // TODO
     return false;
+}
+
+/**
+ * Adds a word to the dictionary.
+ */
+void add_word(const char* word)
+{
+    // just return if word is NULL
+    if (word == NULL)
+    {
+        return;
+    }
+
+    // start at the beginning of dictionary
+    node* curNode = &tree;
+
+    // add each character
+    for (int i = 0; word[i] != '\0'; ++i)
+    {
+        // get character node index
+        int nodeIndex = get_node_index(word[i]);
+
+        // get node for character
+        node* charNode = curNode->children[nodeIndex];
+
+        // create node for character if it doesn't exist
+        if (charNode == NULL)
+        {
+            charNode = malloc(sizeof(tree));
+
+            // initialize new node
+            init_node(charNode, CHILDREN);
+
+            // insert node into dictionary
+            curNode->children[nodeIndex] = charNode;
+        }
+
+        // update current node for next character
+        curNode = charNode;
+    }
+
+    // mark node as a word
+    curNode->data = true;
+}
+
+/**
+ * Returns corresponding node index for given character.
+ */
+int get_node_index(char c)
+{
+    // default return value to apostrophe index
+    int index = CHILDREN - 1;
+
+    if (isalpha(c))
+    {
+        index = c - 'a';
+    }
+
+    return index;
 }
