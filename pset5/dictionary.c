4ab78b12e10a66ebb84baaf1ad2e8251554ad4bd
Amy Bruderer <amy.bruderer@gmail.com>
2015-11-14 16:35:04 -0500

    Changes to appease cs50 auto grader. Moved node implementation into dictionary files.  Deleted node files.

--- pset5/dictionary.c
+++ pset5/dictionary.c
@@ -14,7 +14,6 @@
 #include <string.h>
 
 #include "dictionary.h"
-#include "node.h"
 
 // children per node (one for every lowercase character and apostrophe)
 #define CHILDREN 27
@@ -196,4 +195,53 @@ int get_node_index(char c)
     }
 
     return index;
+}
+
+/**
+ * Initializes node:
+ * - data set to false
+ * - allocate memory for specified number of children node pointers
+ * - each child node pointer set to NULL
+ *
+ * Returns true if successful else false.
+ */
+bool init_node(node* n, int count)
+{
+    // can't initialize NULL pointer
+    if (n == NULL)
+    {
+        return false;
+    }
+
+    n->data = false;
+    n->maxChildren = count;
+    n->children = calloc(count, sizeof(*n->children));
+
+    return (n->children != NULL);
+}
+
+/**
+ * Recursively cleans up memory allocated for this node and all children.
+ * Returns true if successful else false.
+ */
+bool destroy_node(node* n)
+{
+    // no need to destroy NULL pointer
+    if (n == NULL)
+    {
+        return true;
+    }
+
+    // clean up all children
+    for (int i = 0; i < n->maxChildren; ++i)
+    {
+        destroy_node(n->children[i]);
+        free(n->children[i]);
+        n->children[i] = NULL;
+    }
+
+    // clean up pointer to children
+    free(n->children);
+
+    return true;
 }
--- pset5/dictionary.h
+++ pset5/dictionary.h
@@ -36,4 +36,32 @@ unsigned int size(void);
  */
 bool unload(void);
 
+typedef struct node
+{
+    // indicates completion of dictionary key
+    bool data;
+
+    // max number of children
+    int maxChildren;
+
+    // collection of pointers to other nodes
+    struct node** children;
+} node;
+
+/**
+ * Initializes node:
+ * - data set to false
+ * - allocate memory for specified number of children node pointers
+ * - each child node pointer set to NULL
+ *
+ * Returns true if successful else false.
+ */
+bool init_node(node* n, int count);
+
+/**
+ * Recursively cleans up memory allocated for this node and all children.
+ * Returns true if successful else false.
+ */
+bool destroy_node(node* n);
+
 #endif // DICTIONARY_H
